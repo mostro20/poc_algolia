86a3db7ae759d835a3ed6b2bc6c0a3aad7752c07
mostro20 <mostro20@gmail.com>
2020-02-27 14:37:32 +1000

    Changes with all the latest wizzbang features

--- app/src/app.js
+++ app/src/app.js
@@ -6,7 +6,7 @@ const searchClient = algoliasearch(
 );
 
 const search = instantsearch({
-  indexName: 'poc_FederatedQAGOMA',
+  indexName: 'poc_FederatedQAGOMA_v2',
   searchClient,
 });
 
@@ -20,30 +20,38 @@ search.addWidgets([
     templates: {
       item: `
       <div class="card">
-        <a href="{{Image_url}}" target="_blank">
-          <img src="{{Image_url}}" class="card-img-top" align="left" alt="{{name}}" />
+        <a href="{{Multimedia}}" target="_blank">
+          <img src="{{Multimedia}}" class="card-img-top" align="left" alt="{{NamLast}}" />
         </a>
         <div class="card-body">
           <div class="hit-name">
-            <a href="{{Image_url}}">
-              {{#helpers.highlight}}{ "attribute": "Title" }{{/helpers.highlight}}
+            <a href="{{Multimedia}}">
+              {{#helpers.highlight}}{ "attribute": "TitMainTitle" }{{/helpers.highlight}}
             </a>
           </div>
           <div class="hit-description">
-            {{#helpers.highlight}}{ "attribute": "Artist Lastname" }{{/helpers.highlight}}
+            {{#helpers.highlight}}{ "attribute": "NamFirst" }{{/helpers.highlight}}
+            {{#helpers.highlight}}{ "attribute": "NamLast" }{{/helpers.highlight}}<br>
+            {{#helpers.highlight}}{ "attribute": "NamOrganisation" }{{/helpers.highlight}}
           </div>
           <div class="hit-price">{{Date Created}}</div>
         </div>
         <div class="collapse" id="more-details">
           <dl>
-            <dt>Title</dt>
-            <dd>{{Title}}</dd>
+            <dt>Summary</dt>
+            <dd>{{ObjectSummaryData}}</dd>
             <dt>Date Created</dt>
-            <dd>{{Date Created}}</dd>
-            <dt>Credit</dt>
-            <dd>{{Credit}}</dd>
+            <dd>{{CreDateCreated}}</dd>
+            <dt>Artist life (earliest-latest)<dt>
+            <dd>{{BioBirthEarliestDate}} - {{BioDeathEarliestDate}}</dd>
+            <dt>Party type</dt>
+            <dd>{{NamPartyType}}</dd>
             <dt>Accession No.</dt>
-            <dd>{{Accession No}}</dd>
+            <dd>{{TitAccessionNo}}</dd>
+            <dt>Object IRN</dt>
+            <dd>{{ObjectIRN}}</dd>
+            <dt>Credit</dt>
+            <dd>{{AccCreditLineLocal}}</dd>
           </dl>
         </div>
       </div>
@@ -69,44 +77,112 @@ search.addWidgets([
   instantsearch.widgets.hitsPerPage({
   container: '#hits-per-page',
   items: [
-    { label: '9 results per page', value: 9, default: true },
-    { label: '18 results per page', value: 18 },
+    { label: '9 results per page', value: 9 },
+    { label: '18 results per page', value: 18, default: true },
     { label: '27 results per page', value: 27 },
     { label: '36 results per page', value: 36 },
   ],
   }),
 
-  instantsearch.widgets.refinementList({
-    container: '#facet1',
-    attribute: 'Medium',
-    searchable: true,
-    searchablePlaceholder: 'Search media',
-    showMore: true,
-  }),
+
 
   instantsearch.widgets.refinementList({
     container: '#facet2',
-    attribute: 'Date Created',
+    attribute: 'CreDateCreated',
     searchable: true,
+    limit: 5,
     searchablePlaceholder: 'Search year',
     showMore: true,
   }),
 
   instantsearch.widgets.refinementList({
     container: '#facet3',
-    attribute: 'Artist Lastname',
+    attribute: 'CrePlaceOfExecution',
     searchable: true,
+    limit: 5,
     searchablePlaceholder: 'Search artist',
     showMore: true,
   }),
 
   instantsearch.widgets.refinementList({
     container: '#facet4',
-    attribute: 'Country',
+    attribute: 'Country_tab',
     searchable: true,
+    limit: 5,
     searchablePlaceholder: 'Search country',
     showMore: true,
-  })
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet5',
+    attribute: 'NamPartyType',
+    searchable: true,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet6',
+    attribute: 'BioStates_tab',
+    searchable: true,
+    limit: 5,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet7',
+    attribute: 'BioQldArtist',
+    searchable: true,
+    limit: 5,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet8',
+    attribute: 'BioCulturalIdentity1_tab',
+    searchable: true,
+    limit: 5,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet9',
+    attribute: 'BioCulturalIdentity2_tab',
+    searchable: true,
+    limit: 5,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet10',
+    attribute: 'TitDepartment',
+    searchable: true,
+    limit: 5,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet11',
+    attribute: 'PhyMediaCategory',
+    searchable: true,
+    limit: 5,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#facet12',
+    attribute: 'PhySecondaryMediaCategory',
+    searchable: true,
+    limit: 5,
+    searchablePlaceholder: 'Search party type',
+    showMore: true,
+  }),
 ]);
 
 search.start();
